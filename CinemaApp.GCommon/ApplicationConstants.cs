178a2420f7fb2e0c8b62dca60e917fff51e80e83
bpartenova <bpartenova@gmail.com>
2025-06-11 19:06:43 +0300

    adding Movie Form Input Model and Validation Messages

--- CinemaApp.GCommon/ApplicationConstants.cs
+++ CinemaApp.GCommon/ApplicationConstants.cs
@@ -9,5 +9,6 @@ namespace CinemaApp.GCommon
     public static class ApplicationConstants
     {
         public const string AppDateFormat = "yyyy-MM-dd";
+        public const string NoImageUrl = "no-image.jpg";
     }
 }
